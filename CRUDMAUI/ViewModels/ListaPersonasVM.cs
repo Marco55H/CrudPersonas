1872e6ad9b41d092b41eb3bc1fc2265161d74cd7
Marco55H <marco.holguin@iesnervion.es>
2024-11-26 14:41:30 +0100

    EO

---
+++ CRUDMAUI/ViewModels/ListaPersonasVM.cs
@@ -0,0 +1,17 @@
+﻿using CapaBl;
+using CapaEnt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDMAUI.ViewModels
+{
+    internal class ListaPersonasVM
+    {
+
+        private List<ClsPersona> listaPersonas = ClsListadosBDBl.ObtenerLista();
+
+    }
+}
